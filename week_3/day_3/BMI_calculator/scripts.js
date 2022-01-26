374c5713cf9838710fc49fdddff23fef86b13a0f
Amelia <52084092+Am-Scan@users.noreply.github.com>
2022-01-26 19:57:32 +0000

    renamed p id

--- week_3/day_3/BMI_calculator/scripts.js
+++ week_3/day_3/BMI_calculator/scripts.js
@@ -11,13 +11,13 @@ function bmiCalculator(weight,height) {
     
     if (bmi > 24.9) {
         result = `Your BMI is ${bmi}, so you are overweight.`;
-        document.getElementById("demo").innerHTML = result;
+        document.getElementById("results").innerHTML = result;
         
     } else if (bmi < 18.5) {
         result =  `Your BMI is ${bmi}, so you are underweight.`;
-        document.getElementById("demo").innerHTML = result;
+        document.getElementById("results").innerHTML = result;
     } else {
         result =  `Your BMI is ${bmi}, so you are a normal weight.`;
-        document.getElementById("demo").innerHTML = result;
+        document.getElementById("results").innerHTML = result;
     }
 }
