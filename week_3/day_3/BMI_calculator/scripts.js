bfb31e987f5c36f2858696b367175447fb6f56b7
Amelia <52084092+Am-Scan@users.noreply.github.com>
2022-01-26 19:47:45 +0000

    finished BMI

--- week_3/day_3/BMI_calculator/scripts.js
+++ week_3/day_3/BMI_calculator/scripts.js
@@ -1,15 +1,23 @@
+function myFunction() {
+    var x = document.getElementById("input-form");
+    let weight = x.elements[0].value;
+    let height = x.elements[1].value;
+    bmiCalculator(weight,height);
+  }
+
 function bmiCalculator(weight,height) {
     let heightSquared = height * height;
     let bmi = Math.round(weight / heightSquared);
     
     if (bmi > 24.9) {
         result = `Your BMI is ${bmi}, so you are overweight.`;
-        return result; 
+        document.getElementById("demo").innerHTML = result;
+        
     } else if (bmi < 18.5) {
         result =  `Your BMI is ${bmi}, so you are underweight.`;
-        return result; 
+        document.getElementById("demo").innerHTML = result;
     } else {
         result =  `Your BMI is ${bmi}, so you are a normal weight.`;
-        return result; 
+        document.getElementById("demo").innerHTML = result;
     }
 }
