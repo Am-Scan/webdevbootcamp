bc08c59bb75138a9d598fd9606c7431c4fbe6965
Amelia <52084092+Am-Scan@users.noreply.github.com>
2022-01-27 19:17:06 +0000

    Finished dice challenge

---
+++ week_3/day_4/dice_game/scripts.js
@@ -0,0 +1,42 @@
+//Select right area of html
+let playerOneDice = document.getElementsByClassName("img1");
+
+let playerTwoDice = document.getElementsByClassName("img2");
+
+//Randomly select dice image for players (selection function)
+
+function randomNo() {
+    let random1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    return random1;
+}
+
+function chooseDice() {
+    let resultOne = randomNo();
+    playerOneDice[0].setAttribute("src", `images/dice${resultOne}.png`);
+    let resultTwo = randomNo();
+    playerTwoDice[0].setAttribute("src", `images/dice${resultTwo}.png`);
+
+    return {resultOne, resultTwo}
+}
+
+//Calculate a resul
+
+
+let result = chooseDice();
+
+let text;
+
+
+if (result.resultOne < result.resultTwo) {
+    text = "Player 1 loses, Player 2 loses!";
+} else if (result.resultOne > result.resultTwo) {
+    text = "Player 1 wins, Player 2 loses!";
+} else {
+    text = "Draw!";
+}
+
+//Display a result
+
+let resultsText = document.getElementById("results");
+
+resultsText.innerHTML=text;
