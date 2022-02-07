3d3fcba81eb68b1eb64046b18b293db52abd91b6
Amelia <52084092+Am-Scan@users.noreply.github.com>
2022-02-07 19:29:58 +0000

    finished hamburger task

--- week_5/day_1/hamburger/script.js
+++ week_5/day_1/hamburger/script.js
@@ -1,1 +1,8 @@
-bod
+let btn = document.getElementById("btn");
+
+let nav = document.getElementById("nav");
+
+btn.addEventListener("click",()=>{
+    btn.classList.toggle("active");
+    nav.classList.toggle("active");
+});
